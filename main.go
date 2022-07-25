41cb121cd9926364fce258e103314eebda628409
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2022-07-25 23:52:29 +0300

    (feat) added notify endpoint to handle orders

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
 	"log"
 	"net/http"
 )
 
 func main() {
-
 	app, err := firebase.NewApp(context.Background(), nil) // firebase will automatically read auth token from environment variable GOOGLE_APPLICATION_CREDENTIALS
+	client, err := app.Messaging(context.Background())
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
@@ -16,6 +19,27 @@ func main() {
 	http.Handle("/notify", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		switch request.Method {
 		case "POST":
+			var data map[string]string
+			err := json.NewDecoder(request.Body).Decode(&data)
+			if err != nil {
+				log.Printf("error decoding request body: %v\n", err)
+				writer.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			// create a message to send to the device
+			_, err = client.Send(context.Background(), &messaging.Message{
+				Notification: &messaging.Notification{
+					Title: "New order",
+				},
+				Data: map[string]string{
+					"test": "test",
+				},
+				Token: "fcm-token-here"})
+			if err == nil {
+				writer.WriteHeader(http.StatusOK)
+			} else {
+				writer.WriteHeader(http.StatusInternalServerError)
+			}
 
 		}
 	}))
